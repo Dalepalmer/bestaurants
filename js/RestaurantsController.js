c25210b77c1a88a9b6cdb118516c8a77123c7ba9
Dale Palmer and David Zulaica <dap38@zips.uakron.edu>
2015-02-23 12:17:33 -0800

    Allow user to add more properties.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,10 +1,11 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation});
+    $scope.restaurants.push({name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange});
     $scope.restaurantName = null;
     $scope.restaurantCuisine = null;
     $scope.restaurantLocation = null;
+    $scope.restaurantPriceRange = "$";
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
